f5c4ecb01e67bc4b8a8bc8af23350fc8cbadee06
unknown <grant@.CEDRUS.LOCAL>
2010-04-02 15:44:55 -0700

    updated configure options to build for vs2005 instead of just a generic visual studio.  This fixes the issue with lupdate not running on some windows platforms

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 @call "%VS80COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 
-configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 
+configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc2005 -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 -vcproj
 
 nmake
 
