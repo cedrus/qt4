f71bce2e87ff25ad00ee695d5ea22b66d0bc910d
unknown <grant@.CEDRUS.LOCAL>
2010-04-02 17:22:07 -0700

    further updates to the build.bat file on Windows so that the DirectX SDK, and Windows SDK paths are included by default

--- build.bat
+++ build.bat
@@ -1,9 +1,17 @@
 @echo off
 
-@call "%VS80COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+@call "%VS80COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+Set MSSdk=%SYSTEMDRIVE%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
+Set Lib=%MSSdk%\Lib;%Lib%
+Set Include=%MSSdk%\Include;%Include%
+Set CPU=i386
+Set TARGETOS=WINNT
+Set APPVER=5.01
+@call "%DXSDK_DIR%\Utilities\bin\dx_setenv.cmd" x86
 
-configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc2005 -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 -vcproj
 
-nmake
+configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc2005 -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 -vcproj 
+
+REM nmake
 
 %EXTERNAL_SDKS%\setx\setx.exe QTDIR %~dp0 -m
