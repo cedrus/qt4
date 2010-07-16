819d950a7d8675f3526c728a6b813eb5f9ac8f39
Grant Limberg <glimberg@cedrus.com>
2010-07-16 12:03:36 -0700

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-@call "%VS80COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
+@call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
 Set MSSdk=%SYSTEMDRIVE%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2
 Set Lib=%MSSdk%\Lib;%Lib%
 Set Include=%MSSdk%\Include;%Include%
@@ -10,7 +10,7 @@ Set APPVER=5.01
 @call "%DXSDK_DIR%\Utilities\bin\dx_setenv.cmd" x86
 
 
-configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc2005 -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 -vcproj 
+configure.exe -buildkey Cedrus-Qt-4.6.2 -debug-and-release -opensource -platform win32-msvc2010 -webkit -phonon -phonon-backend -mmx -3dnow -sse -sse2 -vcproj 
 
 REM nmake
 
